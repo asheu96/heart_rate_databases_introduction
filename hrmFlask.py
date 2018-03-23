e24803adb10823fac17ac118e0d1d890ba17b848
Alex Sheu <alex.sheu@duke.edu>
2018-03-23 01:01:08 -0400

    test commit

--- hrmFlask.py
+++ hrmFlask.py
@@ -16,16 +16,16 @@ def userHR(email):
         :param email: Email address of the person whose heart rate we want to obtain
         :returns: jsonified data that contains the heart rate of user if user exists - else, tells user that specified user does not exist
         """
-        mail = '{0}'.format(email)
+        mail = "{0}".format(email)
+        print(mail)
         try:
-                u = User.objects.raw({'_id': mail}).first()
+                u = models.User.objects.raw({"_id": mail}).first()
                 data = {"Heart Rate": str(u.heart_rate)}
         except:
                 print('User does not exist')
                 data = {"Heart Rate": 'User does not exist'}
 
         # if user tries to access a user that does not exist, returns a page that tells them the user doesn't exist
-        
         return jsonify(data)
 
 @app.route('/api/heart_rate/average/<email>', methods=['GET'])
@@ -35,13 +35,10 @@ def avgUserHR(email):
         :param email: Email address of the person whose heart rate we want to obtain
         :returns: jsonified data that contains the average heart rate of user if user exists - else, tells user that specified user does not exists
         """
-        mail = '{0}'.format(email)
+        mail = "{0}".format(email)
+        print(mail)
         try:
-                u = User.objects.raw({'_id': mail}).first()
-                hrData = u.heart_rate
-                sum = numpy.sum(hrData)
-                # sums the heart rate data
-                avgHR = double(sum)/double(hrData)
+                avgHR = hr_avg(mail)
                 # gets the average
 
                 data = {"Avg Heart Rate": avgHR}
@@ -58,18 +55,17 @@ def postHR():
         """
         r = request.get_json()
         email = r['user_email']
+        print(email)
         age = r['user_age']
         HR = r['heart_rate']
         time = datetime.datetime.now()
 
         try:
-                u = User.objects.raw({'id': email}).first()
                 add_heart_rate(email, HR, time)
-                data = {'user_email': u.email, 'user_age': u.age, 'heart_rate': u.heart_rate, 'time': time}
-
+                
         except:
                 print('User not found, creating new user')
                 create_user(email, age, HR)
-                data = {'user_email': email, 'user_age': age, 'heart_rate': HR, 'time': time}
-
+                
+        data = {'data': 'NA'}
         return jsonify(data)
